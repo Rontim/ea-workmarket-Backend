21f5c459ebf68d21470fd4b048166877161dcc64
Edwin574 <owinoedwin574@gmail.com>
2023-05-09 15:57:00 +0300

    first commit

--- ea_workmarket/settings.py
+++ ea_workmarket/settings.py
@@ -7,8 +7,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*bs7q91==7k460s0@$so6$d!z5%jq_9@y+8=#8=7rj#)+d!ys7'
