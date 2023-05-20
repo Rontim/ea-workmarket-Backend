61755e03757a4d949d9b9bd2a469d0659ca439a4
Ron Timothy Gitonga <gitonga@ron.Gitonga>
2023-05-20 20:59:44 +0300

    Added a profile update view

--- Account/urls.py
+++ Account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import UserProfileCreateView
+from .views import UserProfileCreateView, UserProfileUpdateView
 
 urlpatterns = [
     path('profile/', UserProfileCreateView.as_view()),
+    path('profile/update/', UserProfileUpdateView.as_view()),
 ]
--- Account/views.py
+++ Account/views.py
@@ -19,3 +19,40 @@ class UserProfileCreateView(APIView):
             return Response(serializer.data, status=201)
 
         return Response(serializer.errors, status=400)
+
+
+class UserProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, format=None):
+        user = request.user
+        try:
+            userProfile = UserProfile.objects.get(user=user)
+        except UserProfile.DoesNotExist:
+            return Response({"detail": "Profile does not exist."}, status=404)
+
+        serializer = UserProfileSerializer(userProfile, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def patch(self, request, format=None):
+        # Get the user associated with the request
+        user = request.user
+
+        try:
+            profile = UserProfile.objects.get(user=user)
+        except UserProfile.DoesNotExist:
+            return Response({"detail": "Profile does not exist."}, status=404)
+
+        serializer = UserProfileSerializer(
+            profile, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
