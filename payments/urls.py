90372376fb07ed3abffb5bfb992731359913f69b
Rontim <rongitonga@gmail.com>
2023-07-01 14:54:27 +0300

    Intergrated paypal as a payment system

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
-
+from .views import PayPalPayment
 
 urlpatterns = [
-
+    path('paypal/payment/', PayPalPayment.as_view()),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,10 +1,21 @@
+
+import json
+from .models import Payment, PaypalDetails, MpesaDetails
+import os
+import base64
+import requests
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework.decorators import api_view
 
-from .models import Payment, PaypalDetails, MpesaDetails
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+APP_SECRET = os.environ.get('APP_SECRET')
+base_url = os.environ.get('base_url')
 
 
 @api_view('POST', 'GET')
@@ -17,3 +28,52 @@ def PaymentSetupView(request):
 
     if request.method == 'GET':
         pass
+
+
+class PayPalPayment(APIView):
+
+    def post(self, request, format=None):
+        auth = base64.b64encode(f'{CLIENT_ID}:{APP_SECRET}'.encode()).decode()
+        headers = {
+            "Authorization": f"Basic {auth}",
+        }
+        data = {
+            "grant_type": "client_credentials",
+        }
+        url = f'{base_url}/v1/oauth2/token'
+        response = requests.post(url, headers=headers, data=data)
+        access_token = response.json()['access_token']
+
+        amount = request.data['amount']
+        freelancer_email = request.data['freelancer']
+        url = f'{base_url}/v2/checkout/orders'
+        try:
+            freelancer = User.objects.get(email=freelancer_email)
+        except User.DoesNotExist:
+            return Response({'error': 'Freelancer not found'})
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}"
+        }
+        payload = {
+            "intent": "CAPTURE",
+            "purchase_units": [
+                {
+                    "amount": {
+                        "value": str(amount),
+                        "currency_code": "USD"
+                    },
+                    "payee": {
+                        "email_address": freelancer.email
+                    }
+                }
+            ],
+            'redirect_urls': {
+                'return_url': "",
+                'cancel_url': "",
+            },
+        }
+
+        response = requests.post(url, headers=headers,
+                                 data=json.dumps(payload))
